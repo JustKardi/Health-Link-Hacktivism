98e0fa6d923ab73f2c4cc6fd8fe6d6d8c77e3604
Nica Zoe <nica.dable@gmail.com>
2024-07-31 21:16:32 +0800

    Added temporary login and signup pages

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,34 @@ const express = require ("express")
 const app = express()
 const path = require ("path")
 
-app.listen(3000, () => {
+const collection = require("./mongodb")
+app.use(express.json())
+
+app.set("views", path.join(__dirname, "../views"));
+app.set("view engine", "hbs");
+
+app.get("/", (req,res)=>{
+    res.render("signup")
+})
+
+app.get("/", (req,res)=>{
+    res.render("login")
+})
+
+
+app.post("/signup", async (req,res)=>{
+    const data = {
+        name: req.body.name,
+        password:req.body.password
+    }
+
+    await collection.insertMany([data])
+
+    res.render("home")
+
+})
+
+// change number in accordance with your local host
+app.listen(5501, () => {
     console.log("port connected");
 })
